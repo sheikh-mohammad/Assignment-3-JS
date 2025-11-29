5ba7fbf44e4b043fd0e1e4013cb3e42f5fc54e1d
Sheikh Mohammad Ahmed <sheikhmohammadahmed07@gmail.com>
2025-11-29 16:11:53 +0500

    Add qualifications array and display logic to Question-7 script.js

---
+++ Chapter-14-to-16-ARRAYS/Question-7/script.js
@@ -0,0 +1,11 @@
+var qualifications = ["SSC", "HSC", "BCS", "BS", "BCOM", "MS", "M. Phil", "PhD"];
+
+document.write("Qualifications:\n");
+document.write(`1) ${qualifications[0]}\n`);
+document.write(`2) ${qualifications[1]}\n`);
+document.write(`3) ${qualifications[2]}\n`);
+document.write(`4) ${qualifications[3]}\n`);
+document.write(`5) ${qualifications[4]}\n`);
+document.write(`6) ${qualifications[5]}\n`);
+document.write(`7) ${qualifications[6]}\n`);
+document.write(`8) ${qualifications[7]}\n`);
