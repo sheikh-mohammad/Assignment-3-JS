405e207535fe628ade8bb4bfadeeea6aa9a02309
Sheikh Mohammad Ahmed <sheikhmohammadahmed07@gmail.com>
2025-11-29 16:19:15 +0500

    Add solution for Chapter 14-16 Question 8: Student scores and percentage calculation

---
+++ Chapter-14-to-16-ARRAYS/Question-8/script.js
@@ -0,0 +1,7 @@
+var studentNames = ["Michael", "John", "Tony"];
+var studentScores = [320, 230, 480];
+var totalMarks = 500;
+
+document.write(`Score of ${studentNames[0]} is ${studentScores[0]}. Percentage: ${(studentScores[0]/totalMarks) * 100} <br/>`);
+document.write(`Score of ${studentNames[1]} is ${studentScores[1]}. Percentage: ${(studentScores[1]/totalMarks) * 100} <br/>`);
+document.write(`Score of ${studentNames[2]} is ${studentScores[2]}. Percentage: ${(studentScores[2]/totalMarks) * 100} <br/>`);
