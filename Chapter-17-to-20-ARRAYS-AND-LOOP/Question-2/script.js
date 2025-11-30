c9041a5a45aaa1d8872d544d1840cbc3fd3576a1
Sheikh Mohammad Ahmed <sheikhmohammadahmed07@gmail.com>
2025-11-30 08:05:37 +0500

    Update JavaScript solution for Chapter 17-20 Question 2 with loop implementation

--- Chapter-17-to-20-ARRAYS-AND-LOOP/Question-2/script.js
+++ Chapter-17-to-20-ARRAYS-AND-LOOP/Question-2/script.js
@@ -1,16 +1,8 @@
 var multidimensionalArray = [[0, 1, 2, 3], [1, 0, 1, 2], [2, 1, 0, 1]];
 
-document.write(`${multidimensionalArray[0][0]} `);
-document.write(`${multidimensionalArray[0][1]} `);
-document.write(`${multidimensionalArray[0][2]} `);
-document.write(`${multidimensionalArray[0][3]} <br/>`);
-
-document.write(`${multidimensionalArray[1][0]} `);
-document.write(`${multidimensionalArray[1][1]} `);
-document.write(`${multidimensionalArray[1][2]} `);
-document.write(`${multidimensionalArray[1][3]} <br/>`);
-
-document.write(`${multidimensionalArray[2][0]} `);
-document.write(`${multidimensionalArray[2][1]} `);
-document.write(`${multidimensionalArray[2][2]} `);
-document.write(`${multidimensionalArray[2][3]}`);
+for (let i = 0; i < multidimensionalArray.length; i++) {
+    for (let j = 0; j < multidimensionalArray[i].length; j++) {
+        document.write(`${multidimensionalArray[i][j]} `);
+    }
+    document.write("<br/>");
+}
