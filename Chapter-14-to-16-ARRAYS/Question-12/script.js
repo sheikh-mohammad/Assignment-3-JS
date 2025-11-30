2cc11e66323a573cd6618cfdf6aef208492b6fbd
Sheikh Mohammad Ahmed <sheikhmohammadahmed07@gmail.com>
2025-11-30 07:44:10 +0500

    Add JavaScript solution for Chapter 14-16 Question 12

---
+++ Chapter-14-to-16-ARRAYS/Question-12/script.js
@@ -0,0 +1,8 @@
+var arr = ["This", "is", "my", "cat"];
+
+var str = arr.join(" ");
+
+document.write(`Array: <br/>`);
+document.write(`${arr} <br/><br/>`);
+document.write(`String: <br/>`);
+document.write(`${str}`);
