8be15934c4e267a9c5442aff62fd485ad8a4e18d
Sheikh Mohammad Ahmed <sheikhmohammadahmed07@gmail.com>
2025-11-30 07:36:56 +0500

    Add JavaScript solution for Chapter 14-16 Question 10

---
+++ Chapter-14-to-16-ARRAYS/Question-10/script.js
@@ -0,0 +1,6 @@
+var studentScores = [320, 230, 480, 120];
+
+var sortedScores = studentScores.sort();
+
+document.write(`Scores of Students: ${studentScores} <br/>`);
+document.write(`Ordered Scores of Students: ${sortedScores}`);
