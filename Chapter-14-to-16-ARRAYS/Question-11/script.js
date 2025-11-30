d651c21d56c47f1749d0ad062da8a351f9ffc3ce
Sheikh Mohammad Ahmed <sheikhmohammadahmed07@gmail.com>
2025-11-30 07:41:24 +0500

    Add JavaScript solution for Chapter 14-16 Question 11

---
+++ Chapter-14-to-16-ARRAYS/Question-11/script.js
@@ -0,0 +1,9 @@
+var cities = ["Karachi", "Lahore", "Islamabad", "Quetta", "Peshawar"];
+
+var selectedCities = cities.slice(2, 4);
+
+document.write(`Cities list: <br/>`);
+document.write(`${cities} <br/> <br/>`);
+
+document.write(`Selected cities list: <br/>`);
+document.write(`${selectedCities}`);
