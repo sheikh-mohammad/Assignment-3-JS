c0258aba876c7d3ec8b4fac1a352b0d5f75a21b1
Sheikh Mohammad Ahmed <sheikhmohammadahmed07@gmail.com>
2025-11-30 08:27:55 +0500

    Add JavaScript solution for Chapter 17-20 Question 6

---
+++ Chapter-17-to-20-ARRAYS-AND-LOOP/Question-6/script.js
@@ -0,0 +1,37 @@
+document.write("<b>Counting:</b> <br/> <br/>");
+
+for (let i = 1; i <= 15; i++) {
+    document.write(`${i}, `);
+}
+
+document.write("<br/> <br/>")
+
+document.write("<b>Reverse counting:</b> <br/> <br/>");
+
+for (let j = 10; j >= 1; j--) {
+    document.write(`${j}, `);
+}
+
+document.write("<br/> <br/>")
+
+document.write("<b>Even:</b> <br/> <br/>");
+
+for (let k = 0; k <= 20; k += 2) {
+    document.write(`${k}, `);
+}
+
+document.write("<br/> <br/>")
+
+document.write("<b>Odd:</b> <br/> <br/>");
+
+for (let l = 1; l < 20; l += 2) {
+    document.write(`${l}, `);
+}
+
+document.write("<br/> <br/>")
+
+document.write("<b>Series:</b> <br/> <br/>");
+
+for (let m = 2; m <= 20; m += 2) {
+    document.write(`${m}k, `);
+}
