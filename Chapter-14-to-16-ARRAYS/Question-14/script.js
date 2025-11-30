4ef0db175b93adcbd5449d36b4927ff27b00fe90
Sheikh Mohammad Ahmed <sheikhmohammadahmed07@gmail.com>
2025-11-30 07:51:40 +0500

    Add JavaScript solution for Chapter 14-16 Question 14

---
+++ Chapter-14-to-16-ARRAYS/Question-14/script.js
@@ -0,0 +1,10 @@
+var devices = ["keyboard", "mouse", "printer", "monitor"];
+
+document.write(`Devices:<br/>${devices} <br/> <br/>`);
+
+devices.reverse();
+
+document.write(`Out:<br/>${devices[0]}<br/>`);
+document.write(`Out:<br/>${devices[1]}<br/>`);
+document.write(`Out:<br/>${devices[2]}<br/>`);
+document.write(`Out:<br/>${devices[3]}<br/>`);
