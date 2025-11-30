27fa0900132a0e7494fedae5cac48f92a28d6a9b
Sheikh Mohammad Ahmed <sheikhmohammadahmed07@gmail.com>
2025-11-30 08:03:38 +0500

    Add JavaScript solution for Chapter 17-20 Question 2

---
+++ Chapter-17-to-20-ARRAYS-AND-LOOP/Question-2/script.js
@@ -0,0 +1,16 @@
+var multidimensionalArray = [[0, 1, 2, 3], [1, 0, 1, 2], [2, 1, 0, 1]];
+
+document.write(`${multidimensionalArray[0][0]} `);
+document.write(`${multidimensionalArray[0][1]} `);
+document.write(`${multidimensionalArray[0][2]} `);
+document.write(`${multidimensionalArray[0][3]} <br/>`);
+
+document.write(`${multidimensionalArray[1][0]} `);
+document.write(`${multidimensionalArray[1][1]} `);
+document.write(`${multidimensionalArray[1][2]} `);
+document.write(`${multidimensionalArray[1][3]} <br/>`);
+
+document.write(`${multidimensionalArray[2][0]} `);
+document.write(`${multidimensionalArray[2][1]} `);
+document.write(`${multidimensionalArray[2][2]} `);
+document.write(`${multidimensionalArray[2][3]}`);
