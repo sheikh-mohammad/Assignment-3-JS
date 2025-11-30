4d83e3d1386305a7672706caf9920bb5cd1ac71c
Sheikh Mohammad Ahmed <sheikhmohammadahmed07@gmail.com>
2025-11-30 07:55:25 +0500

    Add JavaScript solution for Chapter 14-16 Question 15

---
+++ Chapter-14-to-16-ARRAYS/Question-15/script.js
@@ -0,0 +1,10 @@
+var phoneManufacturers = ["Apple", "Samsung", "Motorola", "Nokia", "Sony", "Haier"];
+
+document.write("<select>");
+document.write(`    <option>${phoneManufacturers[0]}</option>`);
+document.write(`    <option>${phoneManufacturers[1]}</option>`);
+document.write(`    <option>${phoneManufacturers[2]}</option>`);
+document.write(`    <option>${phoneManufacturers[3]}</option>`);
+document.write(`    <option>${phoneManufacturers[4]}</option>`);
+document.write(`    <option>${phoneManufacturers[5]}</option>`);
+document.write("</select>");
