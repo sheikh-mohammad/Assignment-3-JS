54da37dc6005dde4a625f0464cfb2a7856e7b4db
Sheikh Mohammad Ahmed <sheikhmohammadahmed07@gmail.com>
2025-11-30 07:47:45 +0500

    Add JavaScript solution for Chapter 14-16 Question 13

---
+++ Chapter-14-to-16-ARRAYS/Question-13/script.js
@@ -0,0 +1,7 @@
+var devices = ["keyboard", "mouse", "printer", "monitor"];
+
+document.write(`Devices:<br/>${devices} <br/> <br/>`)
+document.write(`Out:<br/>${devices[0]}<br/>`);
+document.write(`Out:<br/>${devices[1]}<br/>`);
+document.write(`Out:<br/>${devices[2]}<br/>`);
+document.write(`Out:<br/>${devices[3]}<br/>`);
