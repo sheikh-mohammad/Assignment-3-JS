07e6df1c0a02cace7dd931ec02483b6c7f133845
Sheikh Mohammad Ahmed <sheikhmohammadahmed07@gmail.com>
2025-12-01 20:23:48 +0500

    Add solution for Question 8: Find largest number in array

---
+++ Chapter-17-to-20-ARRAYS-AND-LOOP/Question-8/script.js
@@ -0,0 +1,11 @@
+let A = [24, 53, 78, 91, 12];
+let largestNum = 0;
+
+for (let i = 0; i < A.length; i++) {
+    if (largestNum < A[i]) {
+        largestNum = A[i];
+    }
+}
+
+document.write(`Array item: ${A} <br/>`);
+document.write(`The largest number is ${largestNum}`);
