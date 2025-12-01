031e379ec943d13c449c2e5b5d91826b4fe1d2bf
Sheikh Mohammad Ahmed <sheikhmohammadahmed07@gmail.com>
2025-12-01 21:01:47 +0500

    Add solution for Question 2: Display mobile model and string length

---
+++ Chapter-21-to-25-STRING-METHODS/Question-2/script.js
@@ -0,0 +1,4 @@
+let mobileModel = prompt("Enter Your Favourite Mobile Phone Model:");
+
+document.write(`My favourite phone is: ${mobileModel} <br/>`);
+document.write(`Length of string: ${mobileModel.length}`);
