56e3ab30497a5e077a835aba106000817b6e6db7
Sheikh Mohammad Ahmed <sheikhmohammadahmed07@gmail.com>
2025-12-02 20:26:10 +0500

    Add solution for Question 9 in Chapter 21-25 STRING-METHODS\n\n- Create a JavaScript program that demonstrates type conversion\n- Convert a string number to a numeric value\n- Show the value and type before and after conversion

---
+++ Chapter-21-to-25-STRING-METHODS/Question-9/script.js
@@ -0,0 +1,9 @@
+let value = 472;
+
+document.write(`Value: ${value} <br/>`);
+document.write(`Type: ${typeof value} <br/>`);
+
+value = +value;
+
+document.write(`Value: ${value} <br/>`);
+document.write(`Type: ${typeof value}`);
