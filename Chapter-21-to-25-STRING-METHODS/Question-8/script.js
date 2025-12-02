22015bb665320b6e658062b0a98764de0f7d3224
Sheikh Mohammad Ahmed <sheikhmohammadahmed07@gmail.com>
2025-12-02 20:20:56 +0500

    Add solution for Question 8 - String replacement with replaceAll

---
+++ Chapter-21-to-25-STRING-METHODS/Question-8/script.js
@@ -0,0 +1,4 @@
+var message = "Ali and Sami are best friends. They play cricket andfootball together.";
+
+document.write(`Message: ${message}`);
+document.write(`After replacement: ${message.replaceAll("and", "&")}`)
