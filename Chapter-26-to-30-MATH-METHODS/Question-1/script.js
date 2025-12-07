7b539e07ce4633f44f3a26223615bdcdc94c1611
Sheikh Mohammad Ahmed <sheikhmohammadahmed07@gmail.com>
2025-12-08 00:09:15 +0500

    Add script for Math Methods Question 1: Positive integer rounding functions

---
+++ Chapter-26-to-30-MATH-METHODS/Question-1/script.js
@@ -0,0 +1,6 @@
+let positiveInt = +prompt("Enter Positive Integer");
+
+document.write(`number: ${positiveInt} <br/>`);
+document.write(`round off value: ${Math.round(positiveInt)} <br/>`);
+document.write(`floor value: ${Math.floor(positiveInt)} <br/>`);
+document.write(`ceil value: ${Math.ceil(positiveInt)}`);
