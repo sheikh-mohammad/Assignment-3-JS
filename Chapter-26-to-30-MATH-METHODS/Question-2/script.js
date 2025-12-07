051c3f5d20b4f3a6f6c800f35f92213f1ff7cc8f
Sheikh Mohammad Ahmed <sheikhmohammadahmed07@gmail.com>
2025-12-08 00:14:52 +0500

    Add script for Math Methods Question 2: Negative floating point number rounding functions

---
+++ Chapter-26-to-30-MATH-METHODS/Question-2/script.js
@@ -0,0 +1,6 @@
+let negativeFloat = parseFloat(prompt("Enter Negative Floating Point Number:"));
+
+document.write(`number: ${negativeFloat} <br/>`);
+document.write(`round off value: ${Math.round(negativeFloat)} <br/>`);
+document.write(`floor value: ${Math.floor(negativeFloat)} <br/>`);
+document.write(`ceil value: ${Math.ceil(negativeFloat)}`);
